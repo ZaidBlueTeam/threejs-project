b7e7536d3cec3b8302495ebc85b82134300ec8a3
ZaidBlueTeam <zaidammar864@gmail.com>
2025-10-26 22:28:40 +0100

    Fix webpack publicPath for GitHub Pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,48 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    publicPath: process.env.NODE_ENV === 'production' ? '/threejs-project/' : '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+      publicPath: isProduction ? '/threejs-project/' : '/',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
           },
         },
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      filename: 'index.html',
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'dist'),
+      ],
     },
-    compress: true,
-    port: 3000,
-    open: true,
-  },
-  performance: {
-    hints: false,
-  },
-  mode: 'development',
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        filename: 'index.html',
+      }),
+    ],
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, 'dist'),
+      },
+      compress: true,
+      port: 3000,
+      open: true,
+    },
+    performance: {
+      hints: false,
+    },
+    mode: argv.mode || 'development',
+  };
 };
